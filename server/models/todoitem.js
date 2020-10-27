79dd9ce7da32da4184ebadb3511c02364df6f1c1
Julia Thea <yuliya.boyadjan@gmail.com>
2020-10-27 14:53:24 -0500

    created migrations

--- server/models/todoitem.js
+++ server/models/todoitem.js
@@ -1,24 +1,21 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class TodoItem extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  TodoItem.init({
-    content: DataTypes.STRING,
-    complete: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'TodoItem',
+  const TodoItem = sequelize.define('TodoItem', {
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    complete: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   });
+
+  TodoItem.associate = (models) => {
+    TodoItem.belongsTo(models.Todo, {
+      foreignKey: 'todoId',
+      onDelete: 'CASCADE',
+    });
+  };
+
   return TodoItem;
 };
