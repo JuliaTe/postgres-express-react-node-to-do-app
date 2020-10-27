cb871f1fa5b667027047fe1e1cc25c1e6dcd31f3
Julia Thea <yuliya.boyadjan@gmail.com>
2020-10-27 14:20:45 -0500

    added Todo and TodoItem models

--- bin/www.js
+++
@@ -1,9 +0,0 @@
-// Setting up application entry and application server
-const http = require('http')
-const app = require('../app') // The express app
-
-const port = parseInt(process.env.PORT, 10) | 8000
-app.set('port', port)
-
-const server = ttp.createServer(app)
-erver.listen(port)
---
+++ server/models/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development'; // Reading configuration specific to our node env, if there is none, default to dev
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) { // Use env variables if available, otherwise use params from config file
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    (file.indexOf('.') !== 0) && 
+    (file !== basename) && 
+    (file.slice(-3) === '.js');
+  })
+  .forEachforEach(file => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => { // Applying relationships between models if such relationships exist
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
